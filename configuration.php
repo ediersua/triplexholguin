2fca9ca0ab4e9fa536e71df38970cb36606f009a
Edier Sua <suarivas@gmail.com>
2013-04-04 11:34:42 -0500

    Versión inicial del sitio

---
+++ configuration.php
@@ -0,0 +1,70 @@
+<?php
+class JConfig {
+	public $offline = '1';
+	public $offline_message = '<b>Sitio en construcción</b>  <br />
+Mayor información: <a href="mailto:info@triplexholguin.com">info@triplexholguin.com</a> <br />
+Calle 79B No 53 - 16 <br />
+Tels: 630 1268 - 225 1108 <br />
+Bogotá, Colombia <br />';
+	public $display_offline_message = '1';
+	public $offline_image = '';
+	public $sitename = 'Triplex Holguin SAS';
+	public $editor = 'tinymce';
+	public $captcha = '0';
+	public $list_limit = '20';
+	public $access = '1';
+	public $dbtype = 'mysql';
+	public $host = 'tri1309108061858.db.10827905.hostedresource.com';
+	public $user = 'tri1309108061858';
+	public $password = 'Jklq76@ij';
+	public $db = 'tri1309108061858';
+	public $dbprefix = 'jos_';
+	public $secret = '30ba25d18d597bde50181e63890fabc6';
+	public $gzip = '0';
+	public $error_reporting = 'default';
+	public $helpurl = 'http://help.joomla.org/proxy/index.php?option=com_help&keyref=Help{major}{minor}:{keyref}';
+	public $ftp_host = '';
+	public $ftp_port = '';
+	public $ftp_user = '';
+	public $ftp_pass = '';
+	public $ftp_root = '';
+	public $ftp_enable = '0';
+	public $tmp_path = '/home/content/05/10827905/html/tmp';
+	public $log_path = '/home/content/05/10827905/html/logs';
+	public $live_site = 'http://triplexholguin.com/';
+	public $force_ssl = '0';
+	public $offset = 'UTC';
+	public $lifetime = '15';
+	public $session_handler = 'database';
+	public $mailer = 'mail';
+	public $mailfrom = 'info@triplexholguin.com';
+	public $fromname = 'triplexholguin.com';
+	public $sendmail = '/usr/sbin/sendmail';
+	public $smtpauth = '0';
+	public $smtpuser = '';
+	public $smtppass = '';
+	public $smtphost = 'localhost';
+	public $caching = '0';
+	public $cachetime = '15';
+	public $cache_handler = 'file';
+	public $debug = '0';
+	public $debug_lang = '0';
+	public $MetaDesc = '';
+	public $MetaKeys = 'triplex, holguin, comercializadora, importadora, madera, maderas';
+	public $MetaTitle = '1';
+	public $MetaAuthor = '1';
+	public $MetaVersion = '0';
+	public $robots = '';
+	public $sef = '1';
+	public $sef_rewrite = '1';
+	public $sef_suffix = '0';
+	public $unicodeslugs = '0';
+	public $feed_limit = '10';
+	public $feed_email = 'author';
+	public $smtpsecure = 'none';
+	public $smtpport = '25';
+	public $MetaRights = '';
+	public $sitename_pagetitles = '0';
+	public $cookie_domain = '';
+	public $cookie_path = '';
+}
